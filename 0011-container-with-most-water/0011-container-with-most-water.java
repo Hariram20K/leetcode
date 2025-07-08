d653c2ee1badf5c3bc71dd387325d4bc6cf217d8
Hariram K <154618717+Hariram20K@users.noreply.github.com>
2025-07-08 14:36:21 +0530

    Time: 5 ms (75.48%), Space: 58.1 MB (31.72%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            // Calculate the area between the two pointers
+            int h = Math.min(height[left], height[right]);
+            int w = right - left;
+            int area = h * w;
+
+            // Update max area if current area is larger
+            maxArea = Math.max(maxArea, area);
+
+            // Move the pointer pointing to the shorter line
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
