e6d4bc53a1051a96495d11edca7f060d4408b1ed
Hariram K <154618717+Hariram20K@users.noreply.github.com>
2025-07-08 17:42:30 +0530

    Time: 1 ms (86.32%), Space: 41 MB (53.82%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int mySqrt(int x) {
+        int left = 0;
+        int right = x >> 1;    //same as right = x/2
+
+        if(x < 2)
+            return x;
+        
+        while(left <= right){
+            long mid = (left + right) >> 1;     //same as mid = (left + right) / 2;
+            if(mid * mid == x)
+                return (int)mid;
+            else if(mid * mid < x)
+                left = (int)mid + 1;
+            else
+                right = (int)mid - 1; 
+        }
+        return left - 1;
+    }
+}
